662b04297178431ecbb33b0f9a13261f03d30c35
savelii.sychov <savelii.sychov@myheritage.com>
2019-09-25 18:52:47 +0300

    K_1

--- lections/lection_1_Intro/materials/main.js
+++ lections/lection_1_Intro/materials/main.js
@@ -1,1 +1,83 @@
-console.log('lection 1')
+console.log('lection 1');
+
+
+x = 1;
+y = 20;
+c = x + y;
+
+
+// c = x - y;
+// c = x / y;
+// c = x * y;
+
+// console.log(c);
+
+// -----------------------------------
+
+
+example = 20;
+name = 'Hello world!';
+
+// console.log(example);
+// console.log(name);
+
+// myName = prompt();
+// console.log(myName);
+
+// typeof
+// console.log(typeof myName);
+
+// ------------------------------------
+
+
+// x = '5';
+// y = 4;
+// x = +prompt('x = ');
+// y = +prompt('y = ');
+// 1
+// x = parseInt(x);
+// y = parseInt(y);
+
+// 2
+
+// x = +x;
+// y = +y;
+
+// console.log( 'x =', x, ' y =', y );
+// console.log( 'x - y =' , x - y );
+// console.log( 'x * y =' , x * y );
+// console.log( 'x / y =' , x / y );
+// console.log('-----------------------');
+// console.log( 'x + y =' , x + y );
+
+
+// ------------------------------------
+
+
+x = 1;
+x = x + 1;
+x++;
+++x;
+x += 1;
+
+// console.log('stable - ', x);
+// console.log(++x);
+// console.log('stable - ', x);
+// console.log(x++);
+// 2
+
+
+// smart operations
+// x = 10;
+
+// x += 15;
+// console.log(x);
+// x -= 13;
+// console.log(x);
+// x *= 112;
+// console.log(x);
+// x = x / 19;
+// x /= 19;
+// console.log(x);
+
+// operator %
