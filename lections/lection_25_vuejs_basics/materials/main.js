af8f866d7d08f00f72716cd55c25e0038b770c3c
sychovSaveliy <sychov.saveliy@gmail.com>
2020-01-08 18:36:04 +0200

    L-25

--- lections/lection_25_vuejs_basics/materials/main.js
+++ lections/lection_25_vuejs_basics/materials/main.js
@@ -18,28 +18,29 @@
 
 // // ---------------------------------------
 
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    className: 'active',
-    appearence: 'transition: .5s; color: green;',
-    value: 10,
-    message: 'Вы загрузили эту страницу в: ' + new Date().toLocaleString()
-  }
-})
+// var app2 = new Vue({
+//   el: '#app-2',
+//   data: {
+//     className: 'active',
+//     appearence: 'transition: .5s; color: green;',
+//     value: 10,
+//     message: 'Вы загрузили эту страницу в: ' + new Date().toLocaleString()
+//   }
+// })
 
 // res = appearence  + 'font-size:' + value*3 + 'px';
 
-setInterval(() => {
- app2.value = Math.floor(Math.random() * 60)
-}, 1000)
+// setInterval(() => {
+//  app2.value = Math.floor(Math.random() * 60)
+// }, 1000)
 
 // // // ---------------------------------------
 
 // var app3 = new Vue({
 //   el: '#app-3',
 //   data: {
-//     seen: true
+//     seen: true,
+//     info: 'Hello Info!'
 //   }
 // })
 
@@ -76,10 +77,20 @@ setInterval(() => {
 //       {
 //         text: 'Создать что-нибудь классное'
 //       }
-//     ]
+//     ],
+//     info: {
+//       x: 1,
+//       y: 3,
+//       z: 5,
+//       p: 6,
+//       l: 9
+//     }
 //   }
 // })
 
+// [].forEach(element => {
+  
+// });
 
 
 
@@ -90,30 +101,49 @@ setInterval(() => {
 //   data: {
 //     message: 'Привет, Vue.js!',
 //     counter: 3,
-//     list: [6, 9, 18]
+//     list: [6, 9, 18],
+//     specialNames: [
+//       "super",
+//       "puper",
+//       "trooper",
+//       "cooper",
+//       "dooper"
+//     ],
+//     classNames: []
 //   },
 //   methods: {
-//     Click(val, pos){
-//       console.log('clicked', val, pos);
+//     calculate(value) {
+//       return `<sup>${value*10}</sup>`;
 //     },
-//     calculate(val){
-//       return val*10;
+//     onClick(event, id) {
+//       // console.log('clicked', arguments, id);
+//       this.counter++;
+//     },
+//     addRandClass() {
+//       let special = this.specialNames[Math.floor(Math.random()*this.specialNames.length)];
+      
+//       this.classNames.push(special);
 //     }
 //   }
 // })
 
 // // // ---------------------------------------
 
-// var app6 = new Vue({
-//   el: '#app-6',
-//   data: {
-//     message: 'Привет, Vue!',
-//     result: 'RESULT',
-//     x: 10,
-//     switcher: false,
-//     date: ''
-//   }
-// })
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Привет, Vue!',
+    result: 'RESULT',
+    x: 10,
+    switcher: false,
+    date: ''
+  },
+  methods: {
+    onInput(event) {
+      this.message = event.target.value
+    }
+  }
+})
 
 
 // -----------------------------
